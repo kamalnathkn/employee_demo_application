6b415800fbdaef00442bc5335e3fcd60dafb6be6
kamalnathkn <64536036+kamalnathkn@users.noreply.github.com>
2023-04-06 21:05:58 +0530

    updating to merge with latest changes in eclipse

--- src/main/java/com/example/demo/service/employeeservice.java
+++ src/main/java/com/example/demo/service/employeeservice.java
@@ -1,20 +1,29 @@
 package com.example.demo.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.model.Employee;
 import com.example.demo.repo.employeerepo;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
+//import javax.persistence.Query;
+import jakarta.persistence.Query;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
+
+
+
 @Service
 @Transactional
 
 public class employeeservice {
 	 private final employeerepo employeeRepo;
 
+	    @PersistenceContext
+	    private EntityManager entityManager;
+	    
 	    @Autowired
 	    public employeeservice(employeerepo employeeRepo) {
 	        this.employeeRepo = employeeRepo;
@@ -34,5 +43,19 @@ public class employeeservice {
 	    public void deleteEmployee(Long id){
 	        employeeRepo.deleteEmployeeById(id);
 	    }
+	    
+	    public Optional<Employee> findById(Long id){
+	        return employeeRepo.findById(id);
+	    }
+	    
+	    public String tocheck(){
+	        return "insevicelayer";
+	    }
+	   public List<Employee> queryexecution()
+	   {
+		   String sql = "SELECT * FROM EMPLOYEE";
+		   Query query = entityManager.createNativeQuery(sql);
+		   return query.getResultList();
+	   }
 	}
 
