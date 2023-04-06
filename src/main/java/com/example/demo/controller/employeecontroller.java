e592b3aa779dceb2057f9d7c63a22a31d9129396
kamalnathkn <64536036+kamalnathkn@users.noreply.github.com>
2023-04-06 21:04:29 +0530

    updating the query execution part

--- src/main/java/com/example/demo/controller/employeecontroller.java
+++ src/main/java/com/example/demo/controller/employeecontroller.java
@@ -1,68 +1,61 @@
-package com.example.demo.controller;
-
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+package com.example.demo.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.example.demo.model.Employee;
-import com.example.demo.service.employeeservice;
+import com.example.demo.repo.employeerepo;
 
-@RestController
-@RequestMapping("/employee")
-public class employeecontroller {
-	private final employeeservice employeeService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 
-    public employeecontroller(employeeservice employeeService) {
-        this.employeeService = employeeService;
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<List<Employee>> getAllEmployees () {
-        List<Employee> employees = employeeService.findAllEmployees();
-        System.out.println("in line 31");
-        return new ResponseEntity<>(employees, HttpStatus.OK);
-    }
+//import javax.persistence.Query;
+import jakarta.persistence.Query;
+import java.util.List;
+import java.util.Optional;
 
 
-    @GetMapping("/all2")
-    public List<Employee> getAllEmployees2 () {
-        List<Employee> employees = employeeService.findAllEmployees();
-        System.out.println("in line 33");
-        return (employees);
-    }
-    @GetMapping("/all1")
-    public String getAllEmployees1 () {
-//        List<Employee> employees = employeeService.findAllEmployees();
-        System.out.println("in line 32");
-        return("employees1");
-    }
-    
-    @GetMapping("/gitpush")
-    public String getAllEmployees4 () {
-//        List<Employee> employees = employeeService.findAllEmployees();
-        System.out.println("in line 32");
-        return("employees1");
-    }
-    
-    @PutMapping("/update")
-    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
-        Employee updateEmployee = employeeService.updateEmployee(employee);
-        return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
-    }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
-        employeeService.deleteEmployee(id);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-}
+@Service
+@Transactional
+
+public class employeeservice {
+	 private final employeerepo employeeRepo;
+
+	    @PersistenceContext
+	    private EntityManager entityManager;
+	    
+	    @Autowired
+	    public employeeservice(employeerepo employeeRepo) {
+	        this.employeeRepo = employeeRepo;
+	    }
+
+	   
+	    public List<Employee> findAllEmployees() {
+	        return employeeRepo.findAll();
+	    }
+
+	    public Employee updateEmployee(Employee employee) {
+	        return employeeRepo.save(employee);
+	    }
+
+	    
+
+	    public void deleteEmployee(Long id){
+	        employeeRepo.deleteEmployeeById(id);
+	    }
+	    
+	    public Optional<Employee> findById(Long id){
+	        return employeeRepo.findById(id);
+	    }
+	    
+	    public String tocheck(){
+	        return "insevicelayer";
+	    }
+	   public List<Employee> queryexecution()
+	   {
+		   String sql = "SELECT * FROM EMPLOYEE";
+		   Query query = entityManager.createNativeQuery(sql);
+		   return query.getResultList();
+	   }
+	}
 
